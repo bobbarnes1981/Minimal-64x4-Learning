ad40dea2d7a38df7250631412ea15308c600f150
bobbarnes1981 <robertbarnes1981@googlemail.com>
2024-05-12 10:29:07 +0100

    Add init files to copy/compile required tools

---
+++ init_lin.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+LOCATION="../Minimal-64x4-Home-Computer"
+
+#cp $LOCATION/Revision 1.1/FLASH Images/flash.bin .
+#cp $LOCATION/Support\Emulator\Minimal64x4.exe .
+
+g++ $LOCATION/Support/Assembler/asm.cpp -O2 -o ./asm -s -static
--- init_win.ps1
+++ init_win.ps1
@@ -1,5 +1,6 @@
 $location = "..\Minimal-64x4-Home-Computer"
 
-Copy-Item "$location\Support\Emulator\Minimal64x4.exe" .
-Copy-Item "$location\Support\Assembler\asm.exe" .
 Copy-Item "$location\Revision 1.1\FLASH Images\flash.bin" .
+Copy-Item "$location\Support\Emulator\Minimal64x4.exe" .
+
+Copy-Item "$location\Support\Assembler\asm.exe" .
