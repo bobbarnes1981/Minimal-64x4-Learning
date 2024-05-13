adb11dee8d751add153b6933782bb1d0a552d350
bobbarnes1981 <robertbarnes1981@googlemail.com>
2024-05-13 07:30:48 +0100

    Update slashes

--- init_lin.sh
+++ init_lin.sh
@@ -2,6 +2,6 @@
 LOCATION="../Minimal-64x4-Home-Computer"
 
 #cp $LOCATION/Revision 1.1/FLASH Images/flash.bin .
-#cp $LOCATION/Support\Emulator\Minimal64x4.exe .
+#cp $LOCATION/Support/Emulator/Minimal64x4.exe .
 
 g++ $LOCATION/Support/Assembler/asm.cpp -O2 -o ./asm -s -static
